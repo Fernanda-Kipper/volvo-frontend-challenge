1e23c9b0a6907852bd327c6ec3ef25eb5f199dd5
Fernanda Kipper <nanda.kipper@gmail.com>
2023-10-04 20:25:25 -0300

    feat: add mobile pagination

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text } from "vcc-ui";
 import { useCars } from "../hooks/useCars";
 import { CarCard } from "./CarCard";
@@ -6,9 +6,11 @@ import { CarCard } from "./CarCard";
 import styles from '../../public/css/home.module.css'
 import { Spacer } from "./Spacer";
 import PaginationDesktop from "./PaginationDesktop";
+import PaginationMobile from "./PaginationMobile";
 
 export const HomeComponent: React.FC = () => {
   const { cars } = useCars();
+  const [selected, setSelected ]= useState(0)
 
   const onClickNavigate = (left: boolean) => {
     let cardList = document.getElementById("card-list");
@@ -21,6 +23,17 @@ export const HomeComponent: React.FC = () => {
     else cardList?.scrollTo({ left: scrollPosition + cardSize })
   }
 
+  const onClickMobile = (index: number) => {
+    let cardList = document.getElementById("card-list");
+    let card = cardList?.firstElementChild;
+    let cardSize = (card?.clientWidth ?? 0) + 24;
+    let scrollSize = cardList?.scrollWidth ?? 0;
+    let scrollPosition = cardList?.scrollLeft ?? 0;
+
+    cardList?.scrollTo({ left: scrollPosition + (cardSize * index) })
+    setSelected(index);
+  }
+
   return (
     <div className={styles.homeWrapper}>
       <Text variant="cook">Todos os modelos Recharge</Text>
@@ -29,6 +42,7 @@ export const HomeComponent: React.FC = () => {
         {cars.map(car => <CarCard key={car.id} car={car}/>)}
       </div>
       <PaginationDesktop onClickLeft={() => onClickNavigate(true)} onClickRight={() => onClickNavigate(false)}/>
+      <PaginationMobile selected={selected} onClick={onClickMobile} total={cars.length}></PaginationMobile>
     </div>
   );
 };
---
+++ src/components/PaginationMobile.tsx
@@ -0,0 +1,23 @@
+import styles from '../../public/css/components/PaginationMobile.module.css'
+
+interface PaginationMobileProps {
+    total: number,
+    onClick: (i: number) => void
+    selected: number;
+}
+
+export default function PaginationMobile({ total, onClick, selected } : PaginationMobileProps){
+    return(
+        <div className={styles.btnWrapper}>
+            {Array
+                .from({ length: total })
+                .map((element,i) => 
+                <button
+                    className={i === selected ? styles.btnSelected : styles.btn}
+                    key={i}
+                    onClick={() => onClick(i)}>
+                </button>
+            )}
+        </div>
+    )
+}
