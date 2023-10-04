7384809f6556f4df5c54a4e7c894a2351126c4b8
Fernanda Kipper <nanda.kipper@gmail.com>
2023-10-04 20:04:24 -0300

    fix: navigation

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -10,25 +10,15 @@ import PaginationDesktop from "./PaginationDesktop";
 export const HomeComponent: React.FC = () => {
   const { cars } = useCars();
 
-  const onClickLeft = () => {
-    let cardList = document?.getElementById("card-list");
-    let card = cardList?.firstElementChild;
-    let cardSize = (card?.clientWidth ?? 0) + 24;
-    let scrollPosition = cardList?.scrollLeft ?? 0;
-    if(scrollPosition >= cardSize) {
-      cardList?.scrollTo({ left: scrollPosition - cardSize })
-    }
-  }
-
-  const onClickRight = () => {
+  const onClickNavigate = (left: boolean) => {
     let cardList = document.getElementById("card-list");
     let card = cardList?.firstElementChild;
     let cardSize = (card?.clientWidth ?? 0) + 24;
     let scrollSize = cardList?.scrollWidth ?? 0;
     let scrollPosition = cardList?.scrollLeft ?? 0;
-    if(scrollPosition + cardSize <= scrollSize) {
-      cardList?.scrollTo({ left: scrollPosition + cardSize })
-    }
+
+    if(left) cardList?.scrollTo({ left: scrollPosition - cardSize })
+    else cardList?.scrollTo({ left: scrollPosition + cardSize })
   }
 
   return (
@@ -38,7 +28,7 @@ export const HomeComponent: React.FC = () => {
       <div className={styles.cardsWrapper} id="card-list">
         {cars.map(car => <CarCard key={car.id} car={car}/>)}
       </div>
-      <PaginationDesktop onClickLeft={onClickLeft} onClickRight={onClickRight}/>
+      <PaginationDesktop onClickLeft={() => onClickNavigate(true)} onClickRight={() => onClickNavigate(false)}/>
     </div>
   );
 };
